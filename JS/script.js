f579f76e689a959ccc157774cafd83849d75dea4
ny-Sopheaktra <60089537+ny-Sopheaktra@users.noreply.github.com>
2023-07-25 18:32:41 +0700

    Responsive Code commit

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
 // toggle 
 const menutoggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
 menutoggle.onclick = function () {
   menutoggle.classList.toggle('active');
+  navigation.classList.toggle('active');
 }
 
 // slider
